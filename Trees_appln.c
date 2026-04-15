accf14d17c06aa751fc03eec4962107a965b80ba
620 <01fe24bec309@kletech.ac.in>
2026-04-15 09:22:14 +0530

    Add files via upload
    One Application on Trees

---
+++ Trees_appln.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node *left, *right;
+};
+
+struct node* createNode(int data) {
+    struct node* newnode = (struct node*)malloc(sizeof(struct node));
+    newnode->data = data;
+    newnode->left = newnode->right = NULL;
+    return newnode;
+}
+
+struct node* insert(struct node* root, int data) {
+    if (root == NULL)
+        return createNode(data);
+
+    if (data < root->data)
+        root->left = insert(root->left, data);
+    else
+        root->right = insert(root->right, data);
+
+    return root;
+}
+
+void inorder(struct node* root) {
+    if (root != NULL) {
+        inorder(root->left);
+        printf("%d ", root->data);
+        inorder(root->right);
+    }
+}
+
+int main() {
+    struct node* root = NULL;
+    int choice, val;
+
+    while (1) {
+        printf("\n1.Insert\n2.Display(Inorder)\n3.Exit\n");
+        printf("Enter choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("Enter value: ");
+                scanf("%d", &val);
+                root = insert(root, val);
+                break;
+
+            case 2:
+                printf("Tree elements: ");
+                inorder(root);
+                break;
+
+            case 3:
+                exit(0);
+
+            default:
+                printf("Invalid choice");
+        }
+    }
+}
